4ec4dd0cec36b7098004893eea937fa78b8d93e0
Justin W Soto <soto@pdx.edu>
2021-08-10 17:26:25 -0700

    I think these all work, npm test is being weird again

--- array-map.js
+++ array-map.js
@@ -6,26 +6,35 @@ export function doubleNumbers(arr) {
 }
 
 export function stringItUp(arr){
-  arr.map((num) => toString)
+  arr.map((num) => num.toString())
+  
 }
 
 export function capitalizeNames(arr){
-    const loArr = arr.map((string) => {
+    const lowerCase = arr.map((string) => {
          return string.toLowerCase();
      })
-     return loArr.map((firtCap) => {
-         return firtCap[0].toUpperCase()+ firtCap.slice(1);
-     })
+     return lowerCase.map((firstLetterCap) => {
+         return firstLetterCap[0].toUpperCase()+ firstLetterCap.slice(1);
+     });
    }
 
+
 export function namesOnly(arr){
-  
+  return arr.map(names => names.name);
 }
+
 
 export function makeStrings(arr){
-  
+  return arr.map(names => {
+    if (names.age >= 18)
+    return `${names.name} can go to the Matrix`;
+    else
+    return `${names.name} is under age!!`;
+  });
 }
 
-export function readyToPutInTheDOM(arr){
-  
+export function readyToPutInTheDOM(arr) {
+  return arr.map(names => `<h1>${names.name}</h1>`
+  `<h2>${names.age}</h2>`);
 }
--- array-map.test.js
+++ array-map.test.js
@@ -1,15 +1,128 @@
-import { doubleNumbers, toString } from './array-map.js';
+import { doubleNumbers, stringItUp, capitalizeNames } from './array-map.js';
+import { namesOnly, makeStrings, readyToPutInTheDOM } from './array-map.js';
 
 test('doubles numbers', () => {
-  const input = [2, 5, 100]; // arrange
-  const output = doubleNumbers(input); // act
-  expect(output).toEqual([4, 10, 200]); // assert
+  const input = [2, 5, 100]; 
+  const output = doubleNumbers(input); 
+  expect(output).toEqual([4, 10, 200]); 
 });
 
-test('toString', () => {
+test('string it up', () => {
     const input = [2, 5, 100];
-    const output = toString(input);
-    expect(output).toEqual(["2", "5", "100"])
+    const output = stringItUp(input);
+    expect(output).toEqual(['2', '5', '100'])
 });
 
-test('capitalizeNames', )
+test('capitalize names', () => {
+  const input = ['john', 'JACOB', 'jinGleHeimer', 'schmidt']; 
+  const output = capitalizeNames(input); 
+  expect(output).toEqual(['John', 'Jacob', 'Jingleheimer', 'Schmidt']); 
+});
+
+test('names only', () => {
+  const input = [
+    {
+      name: 'Angelina Jolie',
+      age: 80
+    },
+    {
+      name: 'Eric Jones',
+      age: 2
+    },
+    {
+      name: 'Paris Hilton',
+      age: 5
+    },
+    {
+      name: 'Kayne West',
+      age: 16
+    },
+    {
+      name: 'Bob Ziroll',
+      age: 100
+    }
+  ]; 
+
+  const output = namesOnly(input); 
+  expect(output).toEqual([
+
+    'Angelina Jolie',
+    'Eric Jones',
+    'Paris Hilton',
+    'Kayne West',
+    'Bob Ziroll'
+
+  ]); 
+
+});
+
+test('make strings', () => {
+  const input = [
+    {
+      name: 'Angelina Jolie',
+      age: 80
+    },
+    {
+      name: 'Eric Jones',
+      age: 2
+    },
+    {
+      name: 'Paris Hilton',
+      age: 5
+    },
+    {
+      name: 'Kayne West',
+      age: 16
+    },
+    {
+      name: 'Bob Ziroll',
+      age: 100
+    }
+  ]; 
+  const output = makeStrings(input);
+  expect(output).toEqual([
+
+    'Angelina Jolie can go to The Matrix',
+    'Eric Jones is under age!!',
+    'Paris Hilton is under age!!',
+    'Kayne West is under age!!',
+    'Bob Ziroll can go to The Matrix'
+
+  ]); 
+});
+
+test('ready to put in the DOM', () => {
+  const input = [
+    {
+      name: 'Angelina Jolie',
+      age: 80
+    },
+    {
+      name: 'Eric Jones',
+      age: 2
+    },
+    {
+      name: 'Paris Hilton',
+      age: 5
+    },
+    {
+      name: 'Kayne West',
+      age: 16
+    },
+    {
+      name: 'Bob Ziroll',
+      age: 100
+    }
+  ]; 
+
+  const output = readyToPutInTheDOM(input); 
+  expect(output).toEqual([
+
+    '<h1>Angelina Jolie</h1><h2>80</h2>',
+    '<h1>Eric Jones</h1><h2>2</h2>',
+    '<h1>Paris Hilton</h1><h2>5</h2>',
+    '<h1>Kayne West</h1><h2>16</h2>',
+    '<h1>Bob Ziroll</h1><h2>100</h2>'
+
+  ]);
+});
